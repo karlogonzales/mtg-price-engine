bb24a34615eeff978dc1d40fb9b65eaf0ffe003d
Sebastien Ong Tone <s.ongtone@hotmail.com>
2026-01-26 23:28:37 -0500

    fixing service

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import asyncio
 
-from price_engine import PriceEngine
+from src.price_engine import PriceEngine
 
 app = Flask(__name__, template_folder="templates")
 
